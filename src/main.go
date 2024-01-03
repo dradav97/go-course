d3536d390a49ec97ed4b8db5df13d7347bb35a43
David Vargas <dradav.97@gmail.com>
2024-01-03 08:18:43 -0500

    feat: add aritmetic operators

--- src/main.go
+++ src/main.go
@@ -3,5 +3,40 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola mundo")
+	// Area of a scuare
+	const baseScuare = 10
+	areaScuare := baseScuare * baseScuare
+	fmt.Println("Area scuare: ", areaScuare)
+
+	x := 10
+	y := 50
+
+	// addition
+	result := x + y
+	fmt.Println("Addition: ", result)
+
+	// subtraction
+	result = y - x
+	fmt.Println("Subtraction: ", result)
+
+	//Multiplication
+	result = y * x
+	fmt.Println("Multiplication: ", result)
+
+	// division
+	result = y / x
+	fmt.Println("Division: ", result)
+
+	// Module
+	result = y % x
+	fmt.Println("Module: ", result)
+
+	// Incremental
+	x++
+	fmt.Println("Incremental: ", x)
+
+	// Decremental
+	x++
+	fmt.Println("Decremental: ", x)
+
 }
